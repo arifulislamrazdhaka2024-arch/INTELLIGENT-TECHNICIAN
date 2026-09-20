ab7786d658dbd950e26a27f86cd558978c5470cc
arifulislamrazdhaka2024-arch <arifulislamrazdhaka2024@gmail.com>
2026-09-20 16:26:02 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/INTELLIGENT-TECHNICIAN/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
